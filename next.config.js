1c6fe33dad1d208a16b82415dd55b1d4a2056269
Orli Dun <122529721+Orliluq@users.noreply.github.com>
2023-12-27 20:50:42 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['fakestoreapi.com'],
+  },
+  staticPageGenerationTimeout: 10,
+  redirects: async () => {
+    return [
+      {
+        source: '/product',
+        destination: '/shop/all',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig
